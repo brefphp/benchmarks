93a2a51c4886e6d5be01890f152a6f75a9be0bd8
Matthieu Napoli <matthieu@mnapoli.fr>
2018-05-20 22:42:59 +0200

    Switch to a simpler/faster HTTP handler for PHP

--- bref.php
+++ bref.php
@@ -1,7 +1,20 @@
 <?php
 
-require __DIR__.'/vendor/autoload.php';
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+use Zend\Diactoros\Response\EmptyResponse;
 
-λ(function () {
-    return [];
-});
+require __DIR__ . '/vendor/autoload.php';
+
+$handler = new class implements RequestHandlerInterface
+{
+    public function handle(ServerRequestInterface $request) : ResponseInterface
+    {
+        return new EmptyResponse;
+    }
+};
+
+$app = new \Bref\Application;
+$app->httpHandler($handler);
+$app->run();
